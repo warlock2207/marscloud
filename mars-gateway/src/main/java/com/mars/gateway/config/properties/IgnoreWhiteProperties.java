e71fd64cc32bc620dce470d384fd18bf03516489
ycw <warlock2207>
2021-11-01 23:32:27 +0800

    修改内容

--- mars-gateway/src/main/java/com/mars/gateway/config/properties/IgnoreWhiteProperties.java
+++ mars-gateway/src/main/java/com/mars/gateway/config/properties/IgnoreWhiteProperties.java
@@ -14,7 +14,7 @@ import java.util.List;
  */
 @Configuration
 @RefreshScope
-@ConfigurationProperties(prefix = "ignore")
+@ConfigurationProperties(prefix = "security.ignore")
 public class IgnoreWhiteProperties
 {
     /**
